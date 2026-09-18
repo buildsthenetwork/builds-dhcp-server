5527411a2de0a180a1daef3cfb96daac257a5729
Hunter Treadway <treadwayhunter@gmail.com>
2026-09-18 18:34:28 +0100

    Created func that converts uint32 to addr string.

--- cmd/dhcp-server/main.go
+++ cmd/dhcp-server/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/buildsthenetwork/builds-dhcp-server/internal/inet"
 )
 
 func main() {
 	fmt.Println("Hello World! DHCP")
+	addr := inet.Int_to_addr(0b11000000_10101000_00001010_00000001)
+	fmt.Println(addr)
 }
--- internal/inet/af_inet.go
+++ internal/inet/af_inet.go
@@ -1,1 +1,18 @@
 package inet
+
+import (
+	"strconv"
+)
+
+func Int_to_addr(iaddr uint32) string {
+
+	octet4 := strconv.Itoa(int(iaddr & 0xff))
+	iaddr = iaddr >> 8
+	octet3 := strconv.Itoa(int(iaddr & 0xff))
+	iaddr = iaddr >> 8
+	octet2 := strconv.Itoa(int(iaddr & 0xff))
+	iaddr = iaddr >> 8
+	octet1 := strconv.Itoa(int(iaddr & 0xff))
+
+	return octet1 + "." + octet2 + "." + octet3 + "." + octet4
+}
