374048d6b34ac3e13876de58459413a4de10863f
Hunter Treadway <treadwayhunter@gmail.com>
2026-09-19 09:22:27 +0100

    Made IPv4 to String and reverse.

--- cmd/dhcp-server/main.go
+++ cmd/dhcp-server/main.go
@@ -10,4 +10,6 @@ func main() {
 	fmt.Println("Hello World! DHCP")
 	addr := inet.Int_to_addr(0b11000000_10101000_00001010_00000001)
 	fmt.Println(addr)
+	iaddr := inet.Addr_to_int("192.168.10.1")
+	fmt.Println(iaddr)
 }
--- internal/inet/af_inet.go
+++ internal/inet/af_inet.go
@@ -2,6 +2,7 @@ package inet
 
 import (
 	"strconv"
+	"strings"
 )
 
 func Int_to_addr(iaddr uint32) string {
@@ -15,4 +16,35 @@ func Int_to_addr(iaddr uint32) string {
 	octet1 := strconv.Itoa(int(iaddr & 0xff))
 
 	return octet1 + "." + octet2 + "." + octet3 + "." + octet4
+}
+
+func Addr_to_int(addr string) uint32 {
+	spl := strings.Split(addr, ".")
+
+	// fmt.Println(spl)
+	octet1, err := strconv.Atoi(spl[0])
+	if err != nil {
+		panic(err)
+	}
+	octet2, err := strconv.Atoi(spl[1])
+	if err != nil {
+		panic(err)
+	}
+	octet3, err := strconv.Atoi(spl[2])
+	if err != nil {
+		panic(err)
+	}
+	octet4, err := strconv.Atoi(spl[3])
+	if err != nil {
+		panic(err)
+	}
+
+	octet1 = octet1 << 24
+	octet2 = octet2 << 16
+	octet3 = octet3 << 8
+
+	iaddr := octet1 & octet2 & octet3 & octet4
+
+	return uint32(iaddr)
+
 }
